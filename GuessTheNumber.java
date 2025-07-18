992f2d8c320c09d85089c03fe8021eac94948102
mageshkannan <mageshkannanam@gmail.com>
2025-07-18 10:28:56 +0530

    add: guess the number and provide feedback to user pgm

---
+++ GuessTheNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class GuessTheNumber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int randomNum = (int)(Math.random()*10)+1;
+        System.out.println("RN "+randomNum);
+        int userNum;
+        int guess =0;
+            System.out.println("Guess the number");
+            System.out.println("The generated number is between 1-10");
+
+        do{
+            System.out.print("Enter your number");
+            userNum = sc.nextInt();
+            guess++;
+            if(userNum >randomNum){
+                System.out.println("Too High!!!");
+            }else{
+                System.out.println("Too Low!!!");
+
+            }
+        }while(randomNum!=userNum);
+                System.out.println("Win! you guessed in "+guess+" guesses");
+    }
+}
